0341ef5c579e97125bffafac8c8d530bd6a6efff
RoadtoS7 <kshvv975@gmail.com>
2020-11-21 23:11:44 +0900

    fix #11 토큰 미들웨어 생성

---
+++ middlewares/authuitils.js
@@ -0,0 +1,29 @@
+const responseMessage = require('../../4th_seminar/modules/responseMessage')
+const jwt = require('../modules/jwt');
+const responseMessage = require('../modules/responseMessage');
+const statusCode = require('../modules/statusCode');
+const util = require('../modules/util');
+
+
+const authUtil = {
+    checkToken: async (req, res, next) => {
+        var token = req.headers.jwt;
+        if (!token) {
+            return res.statusCode(statusCode.BAD_REQUEST).send(util.fail(statusCode.BAD_REQUEST, responseMessage.EMPTY_TOKEN));
+        }
+
+        const user = await jwt.verify(token);
+        if (user === TOKEN_EXPIRED) {
+            return res.statusCode(statusCode.UNAUTHORIZED).send(util.fail(statusCode.UNAUTHORIZED, responseMessage.EXPIRED_TOKEN));
+        }
+        if (user === TOKEN_INVALID) {
+            return res.statusCode(statusCode.UNAUTHORIZED).send(util.fail(statusCode.UNAUTHORIZED, responseMessage.INVALID_TOKEN));
+        }
+        if (user === undefined) {
+            return res.statusCode(statusCode.UNAUTHORIZED).send(util.fail(statusCode.UNAUTHORIZED, responseMessage.INVALID_TOKEN));
+        }
+        req.decoded = user;
+        next();
+    }
+}
+module.exports = authUtil;
--- modules/responseMessage.js
+++ modules/responseMessage.js
@@ -1,6 +1,13 @@
 module.exports = {
-    NULL_VALUE: "필요한 값이 없습니다.",
-    OUT_OF_VALUE: "파라미터 값이 잘못 되었습니다.",
+  NULL_VALUE: "필요한 값이 없습니다.",
+  OUT_OF_VALUE: "파라미터 값이 잘못 되었습니다.",
+
+  /* 토큰 */
+  EMPTY_TOKEN: '토큰 값이 없습니다.',
+  EXPIRED_TOKEN: '토큰 값이 만료되었습니다.',
+  INVALID_TOKEN: '유효하지 않은 토큰값입니다.',
+  AUTH_SUCCESS: '인증에 성공했습니다.',
+  ISSUE_SUCCESS: '새로운 토큰이 생성되었습니다.',
 
   /* 서버에러 */
   INTERNAL_SERVER_ERROR: "서버 내부 오류",
