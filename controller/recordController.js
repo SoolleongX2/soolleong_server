f73b881a0bf79b677b26d2ce1a94a724723f0329
GaYoung <kians2@naver.com>
2020-11-22 01:39:27 +0900

    fix #9 get Record finished

--- controller/recordController.js
+++ controller/recordController.js
@@ -1,4 +1,4 @@
-const {Record, Goal, sequelize} = require('../models');
+const {Record, Goal} = require('../models');
 const util = require('../modules/util');
 const sc = require('../modules/statusCode');
 const rm = require('../modules/responseMessage');
@@ -9,8 +9,29 @@ const { response } = require('express');
 module.exports = {
     getLeftDrinks: async (req, res) => {
         try {
+            // 이번주 모든 레코드 가져오기
+            const goal = await Goal.findOne({
+                limit : 1,
+                order : [['createdAt', 'DESC']],
+                where : { UserId : 1},
+            });
+            console.log(goal);
+            const records = await Record.findAll({
+                where : { GoalId : goal.id },
+            })
+            let shotSum = 0
+            records.filter(m => {shotSum += m.alcoholCount});
             
+            console.log(shotSum);
+            let shotleft = goal.alcoholCount - shotSum;
+            if (shotleft < 0) {
+                shotleft = 0;
+            }
+            const bottle = Math.floor(shotleft / 7);
+            const glass = shotleft % 7;
+            const day = await service.todayToDay();
 
+            res.status(sc.OK).send(util.success(sc.OK, rm.RECORD_GET_SUCCESS, {bottle, glass, day}))
         } catch (err) {
             console.error(err);
         }
@@ -20,10 +41,15 @@ module.exports = {
             const {bottle, glass} = req.body;
     
             // 최근 목표 가져오기
-            const startDay = await Goal.max('createdAt', { where : {UserId : 1}});
+            // const startDay = await Goal.max('createdAt', { where : {UserId : 1}});
+            const goal = await Goal.findOne({
+                limit : 1,
+                order : [['createdAt', 'DESC']],
+                where : { UserId : 1},
+            });
             const td = Date.now();
             const today = new Date(td);
-            const day = startDay.getDate() - today.getDate() + 1; 
+            const day = goal.createdAt.getDate() - today.getDate() + 1; 
             
             if(!bottle || !glass) {
                 res.status(sc.BAD_REQUEST).send(util.fail(sc.BAD_REQUEST, rm.NULL_VALUE));
@@ -34,7 +60,8 @@ module.exports = {
             const record = await Record.create({
                 day,
                 alcoholCount,
-                userId : 1
+                userId : 1,
+                GoalId : goal.id,
             })
 
             res.status(sc.OK).send(util.success(sc.OK, rm.RECORD_SUCCESS, record))
